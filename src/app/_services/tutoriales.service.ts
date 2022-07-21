6018df11d66fe2c0202d0a8ebb2c0e6e8ea77bbf
JhamilRuben <64047231+JhamilRuben@users.noreply.github.com>
2022-07-21 17:12:17 -0500

    Update tutoriales.service.ts

--- src/app/_services/tutoriales.service.ts
+++ src/app/_services/tutoriales.service.ts
@@ -9,7 +9,7 @@ import { Tutorial } from '../_models/tutorial.model';
 })
 export class TutorialesService {
 
-  private baseUrl = 'http://localhost:9090/api/tutorials/';
+  private baseUrl = 'http://ec2-18-206-140-180.compute-1.amazonaws.com:9090/api/tutorials/';
 
   constructor(private http: HttpClient) { }
  
